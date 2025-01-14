8c15c6cb71a5e2dd4ad8f0daf360318343c1e82d
zaki <mzakiwinanda@gmail.com>
2025-01-14 16:08:59 +0700

    admin baru

--- resources/views/Layouts/Admin/editProduct.blade.php
+++ resources/views/Layouts/Admin/editProduct.blade.php
@@ -18,7 +18,7 @@
 
 <body>
     <div
-        class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-600 text-black dark:text-white">
+        class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-white-600 text-black dark:text-white">
 
         <!-- Header -->
         @component('Layouts.Components.header')
@@ -31,7 +31,7 @@
         <!-- ./Sidebar -->
         <div class="h-full ml-14 mt-14 mb-10 md:ml-64">
             <div class="mt-4 mx-4">
-                <form class="mt-8 space-y-6 p-7 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white"
+                <form class="mt-8 space-y-6 p-7 rounded-md bg-white dark:bg-orange-400 text-black dark:text-white"
                     enctype="multipart/form-data" method="post"
                     action="{{route('product.update',$product->id)}}">
                     @method('PUT')
@@ -44,7 +44,7 @@
                                 class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 dark:text-white sm:text-sm" />
 
                             <span
-                                class="absolute start-0 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs dark:text-gray-200">
+                                class="absolute start-0 top-2 -translate-y-1/2 text-xs text-black-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs dark:text-black-900">
                                 Product Name
                             </span>
                         </label>
@@ -84,7 +84,7 @@
                         <label for="UserEmail"
                             class="relative block w-1/2 overflow-hidden border-b border-gray-200 bg-transparent pt-3 focus-within:border-purple-700 dark:border-gray-700">
                             <select id="UserEmail" placeholder="Category" name="category"
-                                class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 dark:text-gray-400 sm:text-sm">
+                                class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 dark:text-white-400 sm:text-sm">
                                 <option value="{{ $product->category->id }}">{{ $product->category->name }}</option>
                                 @foreach ($categories as $category)
                                     @if ($product->category->name !== $category->name)
@@ -108,7 +108,7 @@
                     </div>
                     <div>
                         <button type="submit"
-                            class="block w-full text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800 p-4 transition hover:scale-105">Update
+                            class="block w-full text-white bg-orange-400 hover:bg-orange-900 focus:ring-4 focus:ring-orange-400 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-orange-400 dark:hover:bg-orange-900focus:outline-none dark:focus:ring-purple-800 p-4 transition hover:scale-105">Update
                         </button>
                     </div>
 
